f20ebdfdb7127a1241a260087071507a2905b00b
ahmedtahir123 <ahmed.tahir000000@gmail.com>
2020-01-08 20:29:39 -0800

    Humart 1st commit

--- src/App.js
+++ src/App.js
@@ -1,109 +1,36 @@
-import React from "react";
-import "./App.css";
-
-import ListItem from './Mylist'
-
-
-
-
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-      items: [],
-      currentItem: {
-        text: "",
-        key: ""
-      }
-    };
-
-
-    this.handleInput = this.handleInput.bind(this);
-    this.addItem = this.addItem.bind(this);
-    this.deleteItem = this.deleteItem.bind(this);
-    this.updateItem = this.updateItem.bind(this);
-
-
-  }
-
-
-
-  handleInput(event) {
-    this.setState({
-      currentItem: {
-        text: event.target.value,
-        key: Date.now()
-      }
-    })
-
-  }
-
-  addItem(event) {
-    event.preventDefault();
-    const newItem = this.state.currentItem;
-    if (newItem.text !== "") {
-      const items = [...this.state.items, newItem];
-      this.setState({
-        items: items,
-        currentItem: {
-          text: '',
-          key: ''
-        }
-      })
-    }
-  };
-
-
-
-  deleteItem(key) {
-    const filteredItems = this.state.items.filter(item =>
-      item.key !== key);
-    console.log(filteredItems)
-
-    this.setState({
-      items: filteredItems
-    })
-
-  };
-
-
-  updateItem(text, key) {
-    const items = this.state.items;
-    items.map(item => {
-      if (item.key === key) {
-        item.text = text;
-      }
-    })
-    this.setState({
-      items: items
-    })
-
-
-  }
-
-
-  render() {
-    return (
-
-
-      <div className="App">
-        <form id="myform" onSubmit={this.addItem}>
-
-          <input type="text" placeholder="Enter " value={this.state.currentItem.text} onChange={this.handleInput} />
-
-          <button type='submit' >Add</button>
-          <ListItem items={this.state.items} deleteItem={this.deleteItem} updateItem={this.updateItem}></ListItem>
+import React from 'react'
+import './App.css'
+import Header from './Header'
+import LoginHeader from './LoginHeader'
+import Logodiv from './Logodiv'
+import Category from './Category'
+import Maindiv from './Maindiv'
 
 
 
 
-        </form>
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+      <Header/>
+      <LoginHeader/>
+      <Logodiv/>
+      <Category/>
+      <Maindiv/>
       </div>
-
     )
+      
+    
   }
 }
+
+
+
+
+
+
+
+
 
 export default App;
